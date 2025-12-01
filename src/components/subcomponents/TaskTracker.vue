4d4213da36d6fec6db7e85a3044673b4b9bd7301
feliciaor2024-arch <felicia.or.2024@smu.edu.sg>
2025-12-01 04:53:11 +0000

    ex4 (2)

--- src/components/subcomponents/TaskTracker.vue
+++ src/components/subcomponents/TaskTracker.vue
@@ -1,17 +1,22 @@
 <script>
     // TODO: Complete the code
     export default { 
-        data() {
-            return {
-                
-            }
-        },
+        emit: ['deleteTask'],
+        props: ['task']
         // Hint: insert properties task (Object) and idx (Number)
     }
 </script>
 
 <template>
     <!-- TODO: add your template code here --> 
+     <div class = "card" style = "width:14rem;height:20rem;display:inline-block;vertical-align: top;">
+        <div class = "card-body">
+            <h5 class = "card-title">Deadline: {{ task.deadline }}</h5>
+            <hr></hr>
+            <p class = "card-text">{{ task.desc }}</p>
+            <button class ="btn btn-primary" @click="$emit('deleteTask')">Done</button>
+        </div>
+     </div>
    
 </template>
 
